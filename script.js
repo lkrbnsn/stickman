b9c1c6aefe83c408711696c344773565c7948b10
lkrbnsn <luke.nenagh@gmail.com>
2014-11-25 22:06:48 +0000

    Added stickman functionality

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
-var position = 0;
+var stickmanWindow = Math.floor((Math.random() * 23)) + 1;
 
-function myFunction(windowNumber) {
+function clickFunction(windowNumber) {
 
 	var allWindows = document.getElementsByClassName("win");
+	// Sets them all to black
 	for (i = 0; i < allWindows.length; i++)
 	{ 
 		allWindows[i].style.background="#000";
 	}
 
+	// Sets the clicked one to yellow
 	allWindows[windowNumber-1].style.background='#e6e65c';
     document.getElementById("demo").innerHTML = "You clicked window " + windowNumber;
-    if(windowNumber == 14)
+
+    // If the clicked one is correct
+    if(windowNumber == stickmanWindow)
     {
-		allWindows[13].style.background="#123456"
+		allWindows[stickmanWindow-1].style.backgroundImage="url('img/man2.png')";
+		document.getElementById("demo").innerHTML = "Success!";
     }
+    // Move the stickman
+    else
+    {
+    	var randomVar = Math.random();
+    	console.log(randomVar);
+    	if (randomVar > 0.7)
+    	{
+    		console.log("hello");
+            console.log(stickmanWindow);
+    		allWindows[stickmanWindow-1].style.background='#e6e65c';
+    		allWindows[stickmanWindow-1].style.backgroundImage="url('img/man2.png')";
+            // pauseComp(500);
+            // allWindows[stickmanWindow-1].style.background='#000';
+            // allWindows[stickmanWindow-1].style.backgroundImage="none";
+    		if(randomVar > 0.85)
+    			stickmanWindow = stickmanWindow + 1;
+    		else
+    			stickmanWindow = stickmanWindow - 1
+    	}
+    }
+}
+
+// Function to pause
+function pauseComp(millis)
+{
+var date = new Date();
+var curDate = null;
+
+do { curDate = new Date(); }
+while(curDate-date < millis);
 }
